76afff768ef52d9ffaf2e811edcd26d57361adf9
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-08-29 10:16:13 +1000

    refactor; remove internal commands from user facing commands

--- src/extension.ts
+++ src/extension.ts
@@ -2,20 +2,20 @@ import * as vscode from 'vscode';
 import { NiiVueEditorProvider } from './editorProvider';
 import { NiiVueWebPanel } from './webPanel';
 import { LinkHoverProvider } from './HoverProvider';
-import { NiiVueDocument } from './document';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(NiiVueEditorProvider.register(context));
 	context.subscriptions.push(vscode.languages.registerHoverProvider('*', new LinkHoverProvider()));
 	context.subscriptions.push(vscode.commands.registerCommand('niiVue.open', async () => {
-		const input = await vscode.window.showInputBox({
+		vscode.window.showInputBox({
 			prompt: 'File Path',
 			placeHolder: 'https://niivue.github.io/niivue-demo-images/mni152.nii.gz'
+		}).then((input) => {
+			if (input) {
+				const uri = vscode.Uri.parse(input);
+				NiiVueWebPanel.createOrShow(context.extensionUri, uri);
+			}
 		});
-		if (input) {
-			const uri = vscode.Uri.parse(input);
-			NiiVueWebPanel.createOrShow(context.extensionUri, uri);
-		}
 	}
 	));
 	context.subscriptions.push(vscode.commands.registerCommand('niiVue.openLink', async (args: any) => {
@@ -23,8 +23,7 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 	));
 	context.subscriptions.push(vscode.commands.registerCommand('niiVue.openLocal', async (args: any) => {
-		const uri = args.resourceUri;
-		vscode.commands.executeCommand('vscode.openWith', uri, "niiVue.default");
+		vscode.commands.executeCommand('vscode.openWith', args.resourceUri, "niiVue.default");
 	}
 	));
 }
