4cca5727b1c978d16602da830a2e2c13b5ab409f
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-10-15 13:05:19 +1000

    file association

--- niivue/main.ts
+++ niivue/main.ts
@@ -6,3 +6,24 @@ const app = document.getElementById('app')
 if (app) {
   render(html`<${App} />`, app)
 }
+
+if ('launchQueue' in window) {
+  launchQueue.setConsumer(async (launchParams: any) => {
+    const { files } = launchParams
+    if (files.length > 0) {
+      const fileHandle = files[0]
+      const file = await fileHandle.getFile()
+      const reader = new FileReader()
+      reader.onload = function (event) {
+        window.postMessage({
+          type: 'addImage',
+          body: {
+            data: event.target!.result,
+            uri: fileHandle.name,
+          },
+        })
+      }
+      reader.readAsArrayBuffer(file)
+    }
+  })
+}
