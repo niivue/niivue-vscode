9491b1a016293633e3c558aabb43a4491e92af76
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-10-19 17:55:07 +1000

    mouse event drag

--- niivue/components/Volume.ts
+++ niivue/components/Volume.ts
@@ -1,5 +1,5 @@
 import { html } from 'htm/preact'
-import { useRef } from 'preact/hooks'
+import { useRef, useState } from 'preact/hooks'
 import { NiiVueCanvas } from './NiiVueCanvas'
 import { VolumeOverlay } from './VolumeOverlay'
 import { useSignal } from '@preact/signals'
@@ -19,10 +19,43 @@ export const Volume = ({
   ...props
 }: AppProps & VolumeProps) => {
   const intensity = useSignal('')
-  const volumeRef = useRef()
+  const volumeRef = useRef<HTMLDivElement>()
+  const [isDragging, setIsDragging] = useState(false)
+  const [dragStart, setDragStart] = useState({ x: 0, y: 0 })
+  const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 })
   const dispName = name.length > 20 ? `...${name.slice(-20)}` : name
+
+  const handleMouseDown = (event: MouseEvent) => {
+    volumeRef.current!.style.zIndex = '1'
+    setIsDragging(true)
+    setDragStart({ x: event.clientX, y: event.clientY })
+    setDragOffset({ x: 0, y: 0 })
+  }
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (isDragging) {
+      const dx = event.clientX - dragStart.x
+      const dy = event.clientY - dragStart.y
+      volumeRef.current!.style.left = `${dragOffset.x + dx}px`
+      volumeRef.current!.style.top = `${dragOffset.y + dy}px`
+    }
+  }
+
+  const handleMouseUp = () => {
+    setIsDragging(false)
+    volumeRef.current!.style.left = ''
+    volumeRef.current!.style.top = ''
+    volumeRef.current!.style.zIndex = ''
+  }
+
   return html`
-    <div class="volume" ref=${volumeRef}>
+    <div
+      class="volume"
+      ref=${volumeRef}
+      onMouseDown=${handleMouseDown}
+      onMouseMove=${handleMouseMove}
+      onMouseUp=${handleMouseUp}
+    >
       ${hideUI.value > 0 && html`<div class="volume-name">${dispName}</div>`}
       ${hideUI.value > 0 &&
       html`<button class="volume-remove-button" onclick=${props.remove}>
