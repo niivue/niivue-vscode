4e604c88c874c7f91789ea781895aa6b969c0e7c
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-12-12 13:32:16 +1000

    rearrange homescreen

--- niivue/src/components/HomeScreen.ts
+++ niivue/src/components/HomeScreen.ts
@@ -2,8 +2,19 @@ import { html } from 'htm/preact'
 import imageUrl from '/resources/pwa_install.png'
 
 export const HomeScreen = () => html`
+  <h2 class="text-3xl font-bold text-gray-200 p-2">Drop Files to load images</h2>
+  <p class="w-96 mb-4 text-m font-normal text-gray-300 pl-2">
+    Drag and drop files to an empty space on this window. Many medical image and mesh formats are
+    supported.
+  </p>
+  <h2 class="text-3xl font-bold text-gray-200 p-2">Install as local App</h2>
+  <p class="w-96 mb-4 text-m font-normal text-gray-300 pl-2">
+    <img class="float-right" src=${imageUrl} style="width:150px;height:90px;" />
+    To install niivue-vscode as a local app, click the install button in the address bar.
+    <i> This is currently only supported in chromium-based browsers.</i>
+  </p>
   <h2 class="text-3xl font-bold text-gray-200 p-2">Bookmarklet</h2>
-  <p class="w-96 text-m text-gray-300 pl-2">
+  <p class="w-96 mb-4 text-m font-normal text-gray-300 pl-2">
     Drag this link ⇨
     <a
       href="javascript: (() => {
@@ -22,16 +33,5 @@ export const HomeScreen = () => html`
     <a href="https://niivue.github.io/niivue-demo-images/mni152.nii.gz">
       <b> MNI </b>
     </a>
-  </p>
-  <h2 class="text-3xl font-bold text-gray-200 p-2">Drop Files to load images</h2>
-  <p class="w-96 mb-4 text-m font-normal text-gray-300 pl-2">
-    Drag and drop files to an empty space on this window. Many medical image and mesh formats are
-    supported.
-  </p>
-  <h2 class="text-3xl font-bold text-gray-200 p-2">Install as local App</h2>
-  <p class="w-96 mb-4 text-m font-normal text-gray-300 pl-2">
-    <img class="float-right" src=${imageUrl} style="width:150px;height:90px;" />
-    To install niivue-vscode as a local app, click the install button in the address bar.
-    <i> This is currently only supported in chromium-based browsers.</i>
   </p>
 `
