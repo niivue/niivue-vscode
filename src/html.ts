f8f3fd30a882364f42f0400e6040d0831d81f650
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-12-12 16:08:13 +1000

    adjust html structure; - create individual html for vscode and webapp - change vite base path to /niivue/ for github-pages

--- src/html.ts
+++ src/html.ts
@@ -1,19 +1,8 @@
 import * as vscode from 'vscode'
-import { Buffer } from 'buffer'
-
-function getNonce() {
-  let text = ''
-  const possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  for (let i = 0; i < 32; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
-  }
-  return text
-}
 
 export async function getHtmlForWebview(
   webview: vscode.Webview,
-  extensionUri: vscode.Uri
+  extensionUri: vscode.Uri,
 ): Promise<string> {
   const scriptUri = webview.asWebviewUri(
     vscode.Uri.joinPath(extensionUri, 'niivue', 'build', 'assets', 'index.js'),
@@ -21,17 +10,18 @@ export async function getHtmlForWebview(
   const cssUri = webview.asWebviewUri(
     vscode.Uri.joinPath(extensionUri, 'niivue', 'build', 'assets', 'index.css'),
   )
-  const nonce = getNonce() // Whitelist which scripts can be run
-  const noncePlaceholder = '${nonce}'
-
-  const html = await vscode.workspace.fs.readFile(
-    extensionUri.with({ path: extensionUri.path + '/niivue/build/index.html' }),
-  )
-  const scriptUriPlaceholder = '"/assets/index.js"'
-  const cssUriPlaceholder = '"/assets/index.css"'
-  return Buffer.from(html)
-    .toString('utf8')
-    .replace(noncePlaceholder, nonce)
-    .replace(scriptUriPlaceholder, scriptUri.toString())
-    .replace(cssUriPlaceholder, cssUri.toString())
+  return `<!doctype html>
+          <html lang="en">
+            <head>
+              <meta charset="utf-8" />
+              <script type="module" crossorigin src=${scriptUri}></script>
+              <link rel="stylesheet" crossorigin href=${cssUri}>
+            </head>
+            <body class="text-white p-0">
+              <div id="app" class="w-screen h-screen"></div>
+              <script>
+                var vscode = acquireVsCodeApi()
+              </script>
+            </body>
+          </html> `
 }
