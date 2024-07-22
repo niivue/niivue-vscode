ce5aa9bac509d82ec7428bdf83615e637b95a7e5
Korbinian Eckstein <korbinian90@gmail.com>
2024-07-22 13:58:35 +0200

    add comment

--- niivue/test/DiffNames.test.ts
+++ niivue/test/DiffNames.test.ts
@@ -9,6 +9,9 @@ test('diff names of test and test2', () => {
   ).toEqual(['verysuperendlesslongnametest1', 'verysuperendlesslongnametest2'])
   expect(differenceInNames(['test1.nii', 'test2.nii'])).toEqual(['test1', 'test2'])
   expect(differenceInNames(['qsm_test1', 'qsm_test2'])).toEqual(['test1', 'test2'])
+  expect(differenceInNames(['Glu.nii.gz', 'Gln.nii.gz'])).toEqual(['Glu', 'Gln'])
+
+  // for security reasons only the filename is available, but not the folder name
   expect(differenceInNames(['/folder1/qsm_test1', '/folder2/qsm_test2'])).toEqual([
     'folder1 - test1',
     'folder2 - test2',
@@ -17,5 +20,5 @@ test('diff names of test and test2', () => {
     'folder1',
     'folder2',
   ])
-  expect(differenceInNames(['Glu.nii.gz', 'Gln.nii.gz'])).toEqual(['Glu', 'Gln'])
+  
 })
