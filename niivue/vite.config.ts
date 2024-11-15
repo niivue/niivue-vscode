d357d11138a3db72db0be6f446f8c2c0f20b7ab4
korbinian90 <korbinian90@gmail.com>
2024-11-15 15:56:56 +0100

    test icon with higher contrast

--- niivue/vite.config.ts
+++ niivue/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'niivue_icon_transparent2.png',
+            src: 'niivue_icon_transparent_contrast.png',
             sizes: '200x200',
             type: 'image/png',
           },
