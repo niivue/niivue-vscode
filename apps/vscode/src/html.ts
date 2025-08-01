91cdcc1463a45b66878d076e95f9c95570f74d82
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2025-08-01 00:12:42 +0000

    update vscode extension

--- apps/vscode/src/html.ts
+++ apps/vscode/src/html.ts
@@ -5,17 +5,17 @@ export async function getHtmlForWebview(
   extensionUri: vscode.Uri,
 ): Promise<string> {
   const scriptUri = webview.asWebviewUri(
-    vscode.Uri.joinPath(extensionUri, 'niivue', 'build', 'assets', 'index.js'),
+    vscode.Uri.joinPath(extensionUri, 'niivue', 'index.js'),
   )
   const cssUri = webview.asWebviewUri(
-    vscode.Uri.joinPath(extensionUri, 'niivue', 'build', 'assets', 'index.css'),
+    vscode.Uri.joinPath(extensionUri, 'niivue', 'index.css'),
   )
   return `<!doctype html>
           <html lang="en">
             <head>
               <meta charset="utf-8" />
-              <script type="module" crossorigin src=${scriptUri}></script>
-              <link rel="stylesheet" crossorigin href=${cssUri}>
+              <script type="module" crossorigin src="${scriptUri}"></script>
+              <link rel="stylesheet" crossorigin href="${cssUri}">
             </head>
             <body class="text-white p-0">
               <div id="app" class="w-screen h-screen"></div>
